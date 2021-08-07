b4b831b867cb75d71691407e2b88d2e8035ae48a
Travis Staloch <twostepted@gmail.com>
2021-08-06 19:41:05 -0700

    add setup() to build.zig for common operations

--- build.zig
+++ build.zig
@@ -1,5 +1,12 @@
 const std = @import("std");
 
+fn setup(step: *std.build.LibExeObjStep, mode: std.builtin.Mode, target: anytype) void {
+    step.addCSourceFile("src/utils.c", &[_][]const u8{ "-Wall", "-Wextra", "-Werror", "-O3" });
+    step.setTarget(target);
+    step.linkLibC();
+    step.setBuildMode(mode);
+}
+
 pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
@@ -7,32 +14,24 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
 
     const lib = b.addStaticLibrary("zimdjson", "src/main.zig");
-    lib.addCSourceFile("src/utils.c", &[_][]const u8{ "-Wall", "-Wextra", "-Werror", "-O3" });
-    lib.setTarget(target);
-    lib.linkLibC();
-    lib.setBuildMode(mode);
+    setup(lib, mode, target);
     lib.install();
 
     var main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
-    main_tests.addCSourceFile("src/utils.c", &[_][]const u8{ "-Wall", "-Wextra", "-Werror", "-O3" });
-    main_tests.linkLibC();
+    setup(main_tests, mode, target);
+    // main_tests.setFilter("search tape");
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&main_tests.step);
 
     const exe = b.addExecutable("zimdjson", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.addCSourceFile("src/utils.c", &[_][]const u8{ "-Wall", "-Wextra", "-Werror", "-O3" });
-    exe.linkLibC();
+    setup(exe, mode, target);
     exe.install();
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
+    if (b.args) |args|
         run_cmd.addArgs(args);
-    }
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
