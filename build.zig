264189f5828964e3f2ab3fa77aafe1da3113e528
Travis Staloch <twostepted@gmail.com>
2021-08-29 19:34:38 -0700

    use new builder api for adding options

--- build.zig
+++ build.zig
@@ -1,13 +1,11 @@
 const std = @import("std");
 
-fn setup(step: *std.build.LibExeObjStep, mode: std.builtin.Mode, target: anytype, step_128: bool, ondemand: bool, ondemand_read_cap: u16) void {
+fn setup(step: *std.build.LibExeObjStep, mode: std.builtin.Mode, target: anytype, options: *std.build.OptionsStep) void {
     step.addCSourceFile("src/utils.c", &[_][]const u8{ "-Wall", "-Wextra", "-Werror", "-O3" });
     step.setTarget(target);
     step.linkLibC();
     step.setBuildMode(mode);
-    step.addBuildOption(bool, "step_128", step_128);
-    step.addBuildOption(bool, "ondemand", ondemand);
-    step.addBuildOption(u16, "ondemand_read_cap", ondemand_read_cap);
+    step.addOptions("build_options", options);
 }
 
 pub fn build(b: *std.build.Builder) void {
@@ -34,19 +32,24 @@ pub fn build(b: *std.build.Builder) void {
         "the capacity of the ondemand read buffer. defaults to 4096 (4 Kb)",
     ) orelse 4096;
 
+    const options = b.addOptions();
+    options.addOption(bool, "step_128", step_128);
+    options.addOption(bool, "ondemand", ondemand);
+    options.addOption(u16, "ondemand_read_cap", ondemand_read_cap);
+
     const lib = b.addStaticLibrary("simdjzon", "src/simdjzon.zig");
-    setup(lib, mode, target, step_128, ondemand, ondemand_read_cap);
+    setup(lib, mode, target, options);
     lib.install();
 
     var main_tests = b.addTest("src/tests.zig");
-    setup(main_tests, mode, target, step_128, ondemand, ondemand_read_cap);
+    setup(main_tests, mode, target, options);
     // main_tests.setFilter("ondemand array iteration");
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&main_tests.step);
 
     const exe = b.addExecutable("simdjzon", "src/main.zig");
-    setup(exe, mode, target, step_128, ondemand, ondemand_read_cap);
+    setup(exe, mode, target, options);
     exe.install();
 
     const run_cmd = exe.run();
