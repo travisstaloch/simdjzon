206ce0c8392fc70a2842c9e1bcfc1a8246ae8e56
Travis Staloch <1562827+travisstaloch@users.noreply.github.com>
2023-10-18 00:51:30 -0700

    tests: add test-all.sh

---
+++ test/test-all.sh
@@ -0,0 +1,24 @@
+# run tests and make sure
+
+set -xe
+
+args="-freference-trace"
+declare -a options=("-Doptimize=Debug" "-Doptimize=ReleaseSafe") # "-Doptimize=ReleaseFast" "-Doptimize=ReleaseSmall")
+declare -a targets=("native" "aarch64-linux" "native-windows -fwine")
+
+for option in "${options[@]}"; do
+  for target in "${targets[@]}"; do
+    args2="$args $option"
+    targs="-Dtarget=$target -fqemu"
+    zig build test --summary all $args2 $targs
+    zig build test --summary all $args2 $targs -Dstep-128 
+
+    zig build $args2 $targs
+    zig build -Dondemand $args2 $targs
+    zig build -Dstep-128 $args2 $targs
+    zig build -Dondemand -Dstep-128 $args2 $targs
+  done
+done
+
+# wget https://raw.githubusercontent.com/simdjson/simdjson/master/jsonexamples/twitter.json -O test/twitter.json
+
